efab9aeef105053bce483bc13bdd7bb299576c25
Uttam7470 <159400853+Uttam7470@users.noreply.github.com>
2024-03-25 03:29:43 +0000

    commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "uttam@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
